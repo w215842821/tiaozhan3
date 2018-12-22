ed8e720e3f5ea8d2ce6560109bb26f406732e45d
w215842821 <215842821@qq.com>
2018-12-22 11:15:46 +0800

    v0.3

--- calculator.py
+++ calculator.py
@@ -2,6 +2,25 @@
 
 import sys
 import csv
+from collections import namedtuple
+
+IncomeTaxQucikLookupItem = namedtuple(
+    'IncomeTaxQucikLookupItem',
+    ('start_porint', 'tax_rate', 'quick_subtractor')
+    )
+
+INCOME_TAX_START_PORT = 3500
+
+INCOME_TAX_QUCIK_LOOKUP_TABLE = [
+    IncomeTaxQucikLookupItem(8000, 0.45, 13505)
+    IncomeTaxQucikLookupItem(55000, 0.35, 5505)
+    IncomeTaxQucikLookupItem(35000,0.35, 2755)
+    IncomeTaxQucikLookupItem(9000, 0.25, 1500)
+    IncomeTaxQucikLookupItem(4500, 0.2, 555)
+    IncomeTaxQucikLookupItem(1500, 0.1, 105)
+    IncomeTaxQucikLookupItem(0, 0, 0)
+
+]
 
 #处理命令行参数类
 class Args(object):
@@ -85,9 +104,9 @@ class Config(object):
         3.当格式出错时，抛出异常
         '''
 
-
+config = Config()
 #用户数据类，
-class userData(object):
+class UserData(object):
     """docstring for userData"""
     def __init__(self, arg):
         pass
@@ -108,6 +127,40 @@ class IncomeTaxCaculator(object):
     
     def __init__(self):
         self.userdata = userdata
+
+    def calc_soical_insurance_money(income):
+        if income < config.soical_insurance_baseline_low:
+            return config.soical_insurance_baseline_low * config.soical_insurance_tatal_rate
+        elif income < config.soical_insurance_baseline_high:
+            return income * config.soical_insurance_tatal_rate
+        else :
+            return config.soical_insurance_baseline_high * config.soical_insurance_tatal_rate
+
+    def calc_income_tax_and_remain(income):
+
+        soical_insurance_money = calc_soical_insurance_money(income)
+
+        real_income = income - soical_insurance_money 
+        taxable_part = real_income - INCOME_TAX_START_PORT
+
+        for items in INCOME_TAX_QUCIK_LOOKUP_TABLE:
+            if taxable_part > items.start_porint:
+                tax = taxable_part * items.tax_rate - items.quick_subtractor
+                return '{:.2f}'.format(tax), '{:.2f}'.format(real_income -  tax)
+
+        return '0.00', '{:.2f}'.format(real_income)
+
+    def calc_for_all_userdata(imcome):
+        
+
+
+
+
+
+
+        
+
+
         
 
 
